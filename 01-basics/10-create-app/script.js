61b2b8c4960bc40db7f22751bd622718cb9493d8
dragoblued <qwemee@gmail.com>
2025-01-10 00:00:00 +0700

    01-basics Выполнение 2-х задач

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,17 @@
 import { defineComponent, createApp } from 'vue'
+
+ const App = defineComponent({
+    name: 'App',
+    setup () {
+        function formatDate() {
+            return 'Сегодня ' + (new Intl.DateTimeFormat("en", {dateStyle: "long"}).format(new Date()))
+        }
+        return {
+            formatDate,
+        }
+    },
+     
+    template: '<div>{{formatDate()}}</div>',
+ })
+
+createApp(App).mount('#app')
